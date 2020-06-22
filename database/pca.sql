56eeb3888a56449a256e70cf9969e8c95b375431
Harley <harleym7000@gmail.com>
2020-06-22 01:17:44 +0100

    SQL file

--- database/pca.sql
+++ database/pca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 10, 2020 at 12:27 AM
+-- Generation Time: Jun 22, 2020 at 02:16 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.6
 
@@ -110,7 +110,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (8, '2020_06_07_003429_create_news_table', 5),
 (9, '2020_06_07_005158_create_subs_table', 6),
 (10, '2020_06_07_011943_create_contact_us_table', 7),
-(11, '2020_06_07_235310_add_approved_to_events_table', 7);
+(11, '2020_06_07_235310_add_approved_to_events_table', 7),
+(12, '2020_06_15_101419_add_interest_form_approved_to_users', 8);
 
 -- --------------------------------------------------------
 
@@ -170,7 +171,8 @@ INSERT INTO `roles` (`id`, `name`, `created_at`, `updated_at`) VALUES
 (2, 'Author', '2020-06-06 20:37:10', '2020-06-06 20:37:10'),
 (3, 'Committee Member', '2020-06-06 20:37:10', '2020-06-06 20:37:10'),
 (4, 'Event manager', '2020-06-07 22:02:33', '2020-06-07 22:02:33'),
-(5, 'Content Moderator', '2020-06-08 12:48:21', '2020-06-08 12:48:21');
+(5, 'Content Moderator', '2020-06-08 12:48:21', '2020-06-08 12:48:21'),
+(6, 'Registered Interest', '2020-06-15 10:57:13', '2020-06-15 10:57:13');
 
 -- --------------------------------------------------------
 
@@ -191,11 +193,16 @@ CREATE TABLE `role_user` (
 --
 
 INSERT INTO `role_user` (`id`, `role_id`, `user_id`, `created_at`, `updated_at`) VALUES
-(2, 2, 2, '2020-06-06 21:37:11', '2020-06-06 21:37:11'),
 (3, 3, 3, '2020-06-06 21:37:28', '2020-06-06 21:37:28'),
 (5, 4, 4, '2020-06-07 22:02:54', '2020-06-07 22:02:54'),
 (6, 1, 1, '2020-06-08 14:09:20', '2020-06-08 14:09:20'),
-(8, 5, 5, '2020-06-08 14:12:05', '2020-06-08 14:12:05');
+(9, 5, 1, '2020-06-10 21:09:55', '2020-06-10 21:09:55'),
+(14, 3, 11, '2020-06-14 20:40:00', '2020-06-14 20:40:00'),
+(18, 6, 14, '2020-06-16 21:45:38', '2020-06-16 21:45:38'),
+(19, 6, 15, '2020-06-17 14:52:48', '2020-06-17 14:52:48'),
+(20, 6, 16, '2020-06-17 14:56:17', '2020-06-17 14:56:17'),
+(25, 3, 1, '2020-06-20 12:43:01', '2020-06-20 12:43:01'),
+(28, 3, 29, '2020-06-21 16:47:07', '2020-06-21 16:47:07');
 
 -- --------------------------------------------------------
 
@@ -233,10 +240,13 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'Admin', 'admin@admin.com', NULL, '$2y$10$/dIt7cszkUtUEcoHxNeXGuCoGLJ/RNzsrx2rLI.KYuGxlumAdcGTm', NULL, '2020-06-08 13:08:21', '2020-06-08 13:08:21'),
-(2, 'Author', 'author@author.com', NULL, '$2y$10$O4HGo1ooEPda9YH8U2tKCOcMfghEFEaH3mB8vAmT5rccZEvBuj7m6', NULL, '2020-06-06 20:37:11', '2020-06-06 21:01:16'),
 (3, 'Committee Member', 'user@user.com', NULL, '$2y$10$FTjMIFE/rcRRADM.2anzu.lAoIThk61vZ7BgBMuYmJSkizbSYYq.K', NULL, '2020-06-06 20:37:11', '2020-06-07 00:00:15'),
 (4, 'Event Manager', 'event@event.com', NULL, '$2y$10$6U8.g85gMyZQooK1kuPqouMoo0bgKZRmgZemkLVM8I2b7YGItEfWe', NULL, '2020-06-07 21:03:41', '2020-06-07 21:03:41'),
-(5, 'Content Moderator', 'mod@mod.com', NULL, '$2y$10$WWBJkmcpFFQUleKSH6sjEOihBQNX/Hv.1PkOhL6YmSCJwDLfmM5Pq', NULL, '2020-06-08 13:10:30', '2020-06-08 13:10:30');
+(11, 'Test', 'test@test.com', NULL, '$2y$10$9HCosPZdHCh87c.0e8c5OOiymT4QJQXawVeFh6GCyF.jKyyaAzTAq', NULL, '2020-06-14 19:40:00', '2020-06-14 19:40:00'),
+(14, 'Test 2', 'test2@test2.com', NULL, '$2y$10$JfSR1UNDSES5QwKNs69Roe.42tje01Wcd/7BVAD2SXuNwqNr.idd.', NULL, '2020-06-16 20:45:38', '2020-06-16 20:45:38'),
+(15, 'Test 3', 'test3@test3.com', NULL, '$2y$10$./uciVyXGwCeAA0X1eh/vejGqcl5XNGwxRf5UrB8xk6EbPZnsfSnG', NULL, '2020-06-17 13:52:48', '2020-06-17 13:52:48'),
+(16, 'Test 4', 'test4@test4.com', NULL, '$2y$10$eLgrfOpq7Teg/6f.Pm0fEO0MciNQCJdQiPCJPOnYB.5Hee0pXdGLi', NULL, '2020-06-17 13:56:17', '2020-06-17 13:56:17'),
+(29, 'Harley', 'email@email.com', NULL, '$2y$10$76667rlNFvvdcbfpXUBZ/OUXCzOob/UOl9zRuMZVRdYQS6fGxIR9C', NULL, '2020-06-21 15:47:07', '2020-06-21 15:47:07');
 
 --
 -- Indexes for dumped tables
@@ -331,7 +341,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `news`
@@ -343,13 +353,13 @@ ALTER TABLE `news`
 -- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `role_user`
 --
 ALTER TABLE `role_user`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `subs`
@@ -361,7 +371,7 @@ ALTER TABLE `subs`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- Constraints for dumped tables
