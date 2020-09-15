55c47f69241e1136c083e69ffb12befcbdf72d7c
Harleym7000 <harleym7000@gmail.com>
2020-09-16 00:03:19 +0100

    Register page validation added

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,15 +6,14 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\User;
 use App\Role;
-use App\Cause;
 use App\Rules\Name_Validation;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use App\Rules\Script_Validation;
-use App\Rules\Password_Validation;
+use App\Rules\Postcode_Validation;
+use App\Rules\Phone_Vaidation;
 
 class RegisterController extends Controller
 {
@@ -77,15 +76,15 @@ class RegisterController extends Controller
         $validatedData = $request->validate([
             'forename' => ['required', 'max:255', 'min:2', new Script_Validation, new Name_Validation],
             'surname' => ['required', 'max:255', 'min:2', new Script_Validation, new Name_Validation],
-            'address' => ['required', 'max:255', new Script_Validation],
-            'town' => ['required', 'max:255', new Script_Validation],
-            'postcode' => ['required', 'max:255', new Script_Validation],
-            'tel_no' => ['max:255', new Script_Validation],
-            'mob_no' => ['max:255', new Script_Validation],
-            'email' => ['required', 'max:255', new Script_Validation],
+            'address' => ['required', 'min:8', 'max:255', new Script_Validation],
+            'town' => ['required', 'min:2', 'max:255', new Script_Validation],
+            'postcode' => ['required', 'min:7', 'max:8', new Script_Validation, new Postcode_Validation],
+            'tel_no' => ['min:11', 'max:11', new Script_Validation, new Phone_Vaidation],
+            'mob_no' => ['min:11', 'max:11', new Script_Validation, new Phone_Vaidation],
+            'email' => ['required', 'email', 'max:255', new Script_Validation],
             'password' => ['required', 'max:20', 'regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%@~£^&*()-_=+`¬¦?><.,;:]).*$/', 'confirmed', new Script_Validation],
             'password_confirmation' => ['required', 'max:20', 'regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%@~£^&*()-_=+`¬¦?><.,;:]).*$/', new Script_Validation],
-            'causes[]' => [new Script_Validation]
+            'causes.*' => [new Script_Validation]
         ],
         $messages = [
             'password.regex' => 'Passwords must contaain at least 1 capital letter, 1 number and 1 special character (e.g. @#!?%)',
@@ -114,6 +113,7 @@ class RegisterController extends Controller
 
         $userId = $user->id;
         $causes = request('causes');
+        if($causes) {
         foreach($causes as $cause) {
             $causeID = $cause;
             DB::table('cause_user')->insert([
@@ -121,6 +121,7 @@ class RegisterController extends Controller
                 'cause_id' => $causeID
             ]);
         }
+    }
 
         $user->attachRole($role);
         $this->guard()->logout();
