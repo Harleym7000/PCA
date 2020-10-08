a6368cb8fdd40ab10dce7bc5e4880e0858a688c0
Harleym7000 <harleym7000@gmail.com>
2020-10-08 14:38:22 +0100

    Login design changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">{{ __('Committee Members - Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="/login" novalidate>
@@ -40,29 +40,22 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
+                            <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
 
+                                <div class="login-actions">
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
+                                    <a class="btn btn-link" href="/register">
+                                        {{ __('Not a Committee Member? Register here') }}
+                                    </a>
                                 @endif
+                            </div>
                             </div>
                         </div>
                     </form>
