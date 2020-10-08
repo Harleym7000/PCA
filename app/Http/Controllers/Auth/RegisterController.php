af574acbf9b82a490dfa02e2f5b49dbd67193ac4
Harleym7000 <harleym7000@gmail.com>
2020-10-08 22:43:41 +0100

    Register design changes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -74,17 +74,9 @@ class RegisterController extends Controller
     protected function create(Request $request)
     {
         $validatedData = $request->validate([
-            'forename' => ['required', 'max:255', 'min:2', new Script_Validation, new Name_Validation],
-            'surname' => ['required', 'max:255', 'min:2', new Script_Validation, new Name_Validation],
-            'address' => ['required', 'min:8', 'max:255', new Script_Validation],
-            'town' => ['required', 'min:2', 'max:255', new Script_Validation],
-            'postcode' => ['required', 'min:7', 'max:8', new Script_Validation, new Postcode_Validation],
-            'tel_no' => ['min:11', 'max:11', 'regex:/^\d{11}$/', new Script_Validation, new Phone_Vaidation],
-            'mob_no' => ['min:11', 'max:11', 'regex:/^\d{11}$/', new Script_Validation, new Phone_Vaidation],
             'email' => ['required', 'email', 'max:255', new Script_Validation],
             'password' => ['required', 'max:20', 'regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%@~£^&*()-_=+`¬¦?><.,;:]).*$/', 'confirmed', new Script_Validation],
-            'password_confirmation' => ['required', 'max:20', 'regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%@~£^&*()-_=+`¬¦?><.,;:]).*$/', new Script_Validation],
-            'causes.*' => [new Script_Validation]
+            'password_confirmation' => ['required', 'max:20', 'regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%@~£^&*()-_=+`¬¦?><.,;:]).*$/', new Script_Validation]
         ],
         $messages = [
             'password.regex' => 'Passwords must contaain at least 1 capital letter, 1 number and 1 special character (e.g. @#!?%)',
@@ -100,39 +92,16 @@ class RegisterController extends Controller
 
         $user = new User();
         $role = Role::where('name', 'Committee Member')->first();
-
-        $user->firstname = request('forename');
-        $user->surname = request('surname');
-        $user->address = request('address');
-        $user->town = request('town');
-        $user->postcode = request('postcode');
-        $user->tel_no = request('tel_no');
-        $user->mob_no = request('mob_no');
         $user->email = request('email');
         $user->password = Hash::make(request('password'));
 
         $user->save();
-
-        $userId = $user->id;
-        $causes = request('causes');
-        if($causes) {
-        foreach($causes as $cause) {
-            $causeID = $cause;
-            DB::table('cause_user')->insert([
-                'user_id' => $userId,
-                'cause_id' => $causeID
-            ]);
-        }
-    }
-
-        $user->attachRole($role);
-        $this->guard()->logout();
-        return view('auth.login');
-    }
-    else {
-        $causes = DB::table('causes')->get();
+    } else {
         $request->session()->flash('error', 'Error: Passwords do not match');
         return redirect()->back()->withInput($request->except('password'));
+    }
+        $user->attachRole($role);
+        $this->guard()->logout();
+        return view('auth.conf-email');
+    }
 } 
-}
-}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,6 +9,10 @@
                 <div class="card-header">Committee Members - Register</div>
 
                 <div class="card-body">
+                    <div class="text-center">
+                    <h2>Become a Committee Member Today</h2>
+                    <p>It's quick and easy to sign up</p>
+                </div>
                     <form method="POST" action="/register">
                         @csrf
                         <div class="form-group row">
@@ -29,11 +33,10 @@
                     
                         <div class="form-group row">
                             <label for="password" class="col-md-6 col-form-label">Password:</label>
-                            <label for="password-confirm" class="col-md-4 col-form-label">Confirm Password</label>
                         </div>
                         <div class="form-group row">
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Create a Password" required autocomplete="new-password">
+                            <div class="col-md-12">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Password" required autocomplete="new-password">
                     
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -41,9 +44,15 @@
                                     </span>
                                 @enderror
                             </div>
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control @error('password_confirmation') is-invalid @enderror" name="password_confirmation" placeholder="Confirm Password" required autocomplete="new-password">
-                                @error('password_confirmation')
+                        </div>
+                        <div class="form-group row">
+                            <label for="password" class="col-md-6 col-form-label">Confirm Password:</label>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-md-12">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Re-type your Password" required autocomplete="new-password">
+                    
+                                @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
@@ -51,20 +60,22 @@
                             </div>
                         </div>
                         <br>
-                        <div class="col-md-12">
-                        <div class="form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">I have read and agreed to the <a href="">Terms and Conditions</a></label>
-                          </div>
-                        </div>
-                        <br>
                         <div class="form-group">
+                            <div class="text-center">
                             {!! NoCaptcha::display() !!}
+                        </div>
                           </div>
-                        <div class="form-group row mb-0 text-right">
-                            <div class="col-md-3 text-right">
-                                <button type="submit" class="btn btn-primary pull-right">Register</button>
+                          <div class="col-md-12">
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">I have read and agreed to the <a href="">Terms and Conditions</a></label>
+                              </div>
                             </div>
+                            <br>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-12 text-center">
+                                <button type="submit" class="btn btn-primary pull-right">Register</button>
+                        </div>
                         </div>
                     </form>
                 </div>
