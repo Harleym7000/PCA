5b4c678fe80ba1707e1408d17a2700d383a3b221
Harley Mulholland <harleym7000@gmail.com>
2022-07-28 23:28:44 +0100

    Made about page and footer component fully responsive. Created dusk test file for footer component

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -8,11 +8,11 @@
     <div class="row">
 
       <!-- Grid column -->
-      <div class="col-md-4 mx-auto">
+      <div class="col-12 col-md-4 mx-auto mb-2">
 
         <!-- Content -->
         <h5 class="font-weight-bold text-uppercase mt-3 mb-4">What We Do</h5>
-        <p>Portstewart Community Association is a charitable organisation who currently organises and implemets The Red Sails Festival alongside other events and activities for the benefit 
+        <p>Portstewart Community Association is a charitable organisation who currently organises and implemets The Red Sails Festival alongside other events and activities for the benefit
           of the community.</p>
 
       </div>
@@ -21,7 +21,7 @@
       <hr class="clearfix w-100 d-md-none">
 
       <!-- Grid column -->
-      <div class="col-md-2 mx-auto">
+      <div class="col-12 col-md-4 mx-auto">
 
         <!-- Links -->
         <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
@@ -54,13 +54,13 @@
 
 
       <!-- Grid column -->
-      <div class="col-md-2 mx-auto">
+      <div class="col-12 col-md-4 mx-auto mb-2">
 
         <!-- Links -->
         <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Social Media</h5>
             <div class="row justify-content-center">
-            <a href="https://www.facebook.com/PortstewartCommunityAssociation/" target="_blank" class="col-3 filter-white"><img src="/img/facebook.svg" class="h-8"></a>
-            <a href="https://www.instagram.com/portstewart_community/" target="_blank" class="col-3 filter-white"><img src="/img/instagram.svg" class="h-8"></a>
+            <a href="https://www.facebook.com/PortstewartCommunityAssociation/" target="_blank" class="col-2 col-md-3 col-lg-2 filter-white mb-2"><img src="/img/facebook.svg" class="h-8"></a>
+            <a href="https://www.instagram.com/portstewart_community/" target="_blank" class="col-2 col-md-3 col-lg-2 filter-white mb-2"><img src="/img/instagram.svg" class="h-8"></a>
             </div>
       </div>
       <!-- Grid column -->
@@ -72,7 +72,7 @@
   <!-- Footer Links -->
 
   <!-- Call to action -->
-  
+
 
   <hr>
 
@@ -80,9 +80,9 @@
   <div class="footer-copyright text-center py-3">© <?php echo date('Y') ?> Copyright: Portstewart Community Association
     <br> Charity Number: NIC101459
   </div>
-  <div class="text-center pb-5">
-  <a href="" class="underline underline-offset-1">Privacy Policy</a>
-  <a href="" class="underline underline-offset-1 ml-3">Legal Stuff</a>
+  <div class="text-center pb-5 mt-3">
+  <a href="" class="underline underline-offset-1 mt-2">Privacy Policy</a>
+  <a href="" class="underline underline-offset-1 ml-3 mt-2">Legal Stuff</a>
   </div>
   <!-- Copyright -->
 
