0aa80b40e032c383f006c1d6e5c4cb972d94b269
Harley Mulholland <harleym7000@gmail.com>
2022-07-31 00:19:37 +0100

    Added redirect on login tests for each user role

--- tests/Unit/RedirectOnLoginTest.php
+++ tests/Unit/RedirectOnLoginTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class RedirectOnLoginTest extends TestCase
 {
@@ -11,8 +13,45 @@ class RedirectOnLoginTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_admin_user_redirected_to_admin_home_on_login()
     {
-        $this->assertTrue(true);
+        $user = User::factory()->create();
+        $user->roles()->attach(1);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $this->assertAuthenticated();
+        $response->assertRedirect('/admin');
+    }
+
+    public function test_event_manager_redirected_to_event_manager_home_on_login()
+    {
+        $user = User::factory()->create();
+        $user->roles()->attach(2);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $this->assertAuthenticated();
+        $response->assertRedirect('/manage-events');
+    }
+
+    public function test_author_redirected_to_author_home_on_login()
+    {
+        $user = User::factory()->create();
+        $user->roles()->attach(3);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $this->assertAuthenticated();
+        $response->assertRedirect('/manage-news');
     }
 }
